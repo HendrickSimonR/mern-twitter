c8c7e3fb74c777f03846b0420c4c4849329e4f9e
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-12-06 03:46:29 -0500

    mern twitter

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require('passport');
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err)); 
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+  // const user = new User ({
+  //   handle: 'Jimbo',
+  //   email: 'jim@jim.com',
+  //   password: 'password'
+  // }) 
+  // user.save();
   res.send("Hello Hwerld!");
 });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 
 const port = process.env.PORT || 5000;
 
